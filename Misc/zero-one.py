c15f6043302918858815a1c5a4ebf9e28a8ad841
Brady Henderson <bradyhendu@gmail.com>
2024-11-19 11:15:29 -0500

    zero one knapsack

---
+++ Misc/zero-one.py
@@ -0,0 +1,12 @@
+def zero_one_knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, capacity + 1):
+            if weights[i - 1] <= j:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return dp[n][capacity]
+
+#runs in O(nW) time and O(nW) space
